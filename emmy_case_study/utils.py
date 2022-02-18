c6d275578c8f4e2debddb99194a212a8a12ab258
Daniel Hoffmann <daniel.hoffmann41@gmail.com>
2022-02-18 17:01:02 +0100

    docs: add doc string to the nested_dict function

--- emmy_case_study/utils.py
+++ emmy_case_study/utils.py
@@ -16,6 +16,16 @@ def create_dict_from_json_like_string(json_like_string: str) -> dict:
 def find_specific_value_from_nested_dicts(
     comments_dict: dict, key_to_find: str, value=None
 ):
+    """Searches for an value for a specific key within a potential nested dict.
+
+    Args:
+        comments_dict (dict): The dict which will be searched.
+        key_to_find (str): search goal within the nested dict.
+        value (_type_, optional): Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
     for key in comments_dict.keys():
         if type(comments_dict[key]) == dict:
             value = find_specific_value_from_nested_dicts(
