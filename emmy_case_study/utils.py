d4dc8c7c2265f5c31df0ff41d2c4d485e0cffc5c
Daniel Hoffmann <daniel.hoffmann41@gmail.com>
2022-02-21 11:42:49 +0100

    chore: refactor utils functions

--- emmy_case_study/utils.py
+++ emmy_case_study/utils.py
@@ -1,36 +1,23 @@
-import json
+from typing import Any
 
 
-def create_dict_from_json_like_string(json_like_string: str) -> dict:
-    """Create a dict from a json-like string.
-
-    Args:
-        json_like_string (str): string that need to be jsonified.
-
-    Returns:
-        dict: From the json string created dict.
-    """
-    return json.loads(json_like_string)
-
-
-def find_specific_value_from_nested_dicts(
-    comments_dict: dict, key_to_find: str, value=None
-):
+def find_value_in_nested_dicts(
+    dict_to_search: dict, key_to_find: str, data: Any = None
+) -> Any:
     """Searches for a value for a specific key within a potential nested dict.
 
     Args:
-        comments_dict (dict): The dict which will be searched.
+        dict_to_search (dict): The dict which will be searched.
         key_to_find (str): search goal within the nested dict.
-        value (_type_, optional): Defaults to None.
 
     Returns:
-        _type_: _description_
+        data (Any): the searched value for the key input
     """
-    for key in comments_dict.keys():
-        if type(comments_dict[key]) == dict:
-            value = find_specific_value_from_nested_dicts(
-                comments_dict[key], key_to_find, value
-            )
-        elif key == key_to_find:
-            value = comments_dict[key]
-    return value
+    if dict_to_search:
+        for key, val in dict_to_search.items():
+            if isinstance(val, dict):
+                data = find_value_in_nested_dicts(val, key_to_find)
+            elif key == key_to_find:
+                data = val
+                break
+        return data
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,4 @@
-from ..emmy_case_study.utils import (
-    create_dict_from_json_like_string,
-    find_specific_value_from_nested_dicts,
-)
-
-
-def test_create_dict_from_json_like_string():
-    test_dict = create_dict_from_json_like_string(
-        '{"driven_distance" : 7000, "pizzas_delivered" : 3}'
-    )
-    assert type(test_dict) == dict
-    assert test_dict['driven_distance'] == 7000
-    assert test_dict['pizzas_delivered'] == 3
+from ..emmy_case_study.utils import find_value_in_nested_dicts
 
 
 def test_find_specific_value_from_nested_dicts():
@@ -20,5 +8,5 @@ def test_find_specific_value_from_nested_dicts():
         "weather": "okayish",
         "metrics": {"driven_distance": 3500, "pizzas_delivered": 3},
     }
-    assert find_specific_value_from_nested_dicts(normal_dict, "driven_distance") == 7000
-    assert find_specific_value_from_nested_dicts(nested_dict, "driven_distance") == 3500
+    assert find_value_in_nested_dicts(normal_dict, "driven_distance") == 7000
+    assert find_value_in_nested_dicts(nested_dict, "driven_distance") == 3500
